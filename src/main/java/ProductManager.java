3ed0a489f1e9c8caba3b2ca9f8b77962e51e2799
Anna Beloshkurnik <squirrelpochta2@mail.ru>
2022-12-20 20:56:43 +0300

    Format

--- src/main/java/ProductManager.java
+++ src/main/java/ProductManager.java
@@ -6,20 +6,20 @@ public class ProductManager {
         this.repository = repository;
     }
 
-    public void add(Product product){
+    public void add(Product product) {
         repository.save(product);
     }
 
     public Product[] searchBy(String text) {
         Product[] result = new Product[0]; // тут будем хранить подошедшие запросу продукты
-        for (Product product: repository.findAll()) {
+        for (Product product : repository.findAll()) {
             if (matches(product, text)) {
                 Product[] temp = new Product[result.length + 1];
                 for (int i = 0; i < result.length; i++) {
                     temp[i] = result[i];
                 }
                 temp[temp.length - 1] = product;
-                result  = temp;
+                result = temp;
                 // "добавляем в конец" массива result продукт product
             }
         }
